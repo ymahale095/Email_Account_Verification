7d0dac829d5fde397a3c1567bae3f8351ab64ae3
ymahale095 <113272836+ymahale095@users.noreply.github.com>
2024-07-24 18:58:08 +0530

    msg

---
+++ src/main/java/com/xyz/repository/UserRepo.java
@@ -0,0 +1,14 @@
+package com.xyz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xyz.entity.User;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+	public User findByEmail(String emaill);
+
+
+	public User findByVerificationCode(String code);
+	
+}
---
+++ src/main/java/com/xyz/service/UserService.java
@@ -0,0 +1,17 @@
+package com.xyz.service;
+
+import com.xyz.entity.User;
+
+public interface UserService {
+
+	public User saveUser(User user,String url);
+
+	public void removeSessionMessage();
+
+
+
+	public void sendEmail(User user, String path);
+
+	public boolean verifyAccount(String verificationCode);
+
+}
